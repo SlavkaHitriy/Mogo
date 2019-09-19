2ea3da20f2486e73ab4d4db131b802c6e28f09dc
SlavkaHitriy <slavik.nukutuk@gmail.com>
2019-09-19 19:45:59 +0300

    half of project

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+    $('div.about__btn').click(function () {
+        if ($(this).next().is(":visible")) {
+            $(this).next().hide('slow');
+            $(this).removeClass('active');
+        }
+        else {
+            $('div.about__btn-text:visible').hide('slow');
+            $('div.about__btn-text:visible').prev().removeClass('active');
+            $(this).next().show('slow');
+            $(this).toggleClass('active');
+        }
+    });
+
+    $('.comments__slider').slick({
+        dots: false,
+        prevArrow: '<button class="prev-btn"><img src="../images/comments/prev.png" alt=""></button>',
+        nextArrow: '<button class="next-btn"><img src="../images/comments/next.png" alt=""></button>'
+    });
+
+
+})
