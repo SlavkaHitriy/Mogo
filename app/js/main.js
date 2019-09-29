856b39a106ea580e7e78d0f066b38e9312d557f5
SlavkaHitriy <slavik.nukutuk@gmail.com>
2019-09-29 21:07:26 +0300

    update 2.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
 
 
+    $(function () {
+        $("body, .about__btn-text").niceScroll({
+            cursorwidth: "8px",
+            cursorcolor: "#95e1d3"
+        });
+    });
+
+    $('.intro-link').on('click', function () {
+        var top = $('.services').offset().top;
+        $('html, body').animate({
+            scrollTop: top
+        }, 700)
+    });
+
+    $('.work-link').on('click', function () {
+        var top = $('.work').offset().top;
+        $('html, body').animate({
+            scrollTop: top
+        }, 700)
+    });
+
+    $('.about-link').on('click', function () {
+        var top = $('.about').offset().top;
+        $('html, body').animate({
+            scrollTop: top
+        }, 700)
+    });
+
+    $('.contacts-link').on('click', function () {
+        var top = $('footer').offset().top;
+        $('html, body').animate({
+            scrollTop: top
+        }, 1000)
+    });
+
+
     $('div.about__btn').click(function () {
         if ($(this).next().is(":visible")) {
             $(this).next().hide('slow');
@@ -27,7 +63,7 @@ $(document).ready(function(){
     });
 
 
-    $('.burger').on('click', function(){
+    $('.burger-menu').on('click', function(){
         $('.menu-list').toggleClass('active');
     })
 
